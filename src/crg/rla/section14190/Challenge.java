38a8b7a9f4d4f39250b741936507931a1239f32d
LanCorC <lanc3.corcega@gmail.com>
2023-03-19 18:55:06 +1300

    Completing Section 14.193: Potential issue with equals() and subclassing
    note: all sections 12+ i complete has is not the latest 2023 re-master, still pending

---
+++ src/crg/rla/section14190/Challenge.java
@@ -0,0 +1,23 @@
+package crg.rla.section14190;
+
+public class Challenge {
+    /**
+     * When overriding equals(), careful ensure NOT TRUE if HeavenlyBody was
+     * compared to subclass of itself
+     *
+     * showed in demonstration within this chapter, but unnecessary in the
+     * HeavenlyBody class
+     *
+     * mini challenge is this one question: why was it unnecessary?
+     *
+     * hint: check lecture 97
+     *
+     * */
+
+    /**
+     * My answer:
+     * It cannot be subclassed - HeavenlyBody declaration has FINAL
+     * keyword
+     */
+
+}
